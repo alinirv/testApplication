8368f144f0e9319df1b225fda73039add0caf085
alinirv <alini.vicente@yahoo.com.br>
2023-09-24 14:41:45 -0300

    test: add value to method.

--- src/test/java/br/edu/ifsp/GiftCalculatorTest.java
+++ src/test/java/br/edu/ifsp/GiftCalculatorTest.java
@@ -2,15 +2,19 @@ package br.edu.ifsp;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+
 import static org.assertj.core.api.Assertions.*;
 
 class GiftCalculatorTest {
     @Test
     @DisplayName("Must balance gifts")
     void mustBalanceGifts(){
+        int resultOfComparingSides= 5;
         GiftCalculator calculator= new GiftCalculator();
-        calculator.calculateWeight();
-        assertThat(calculator.checksBalance().isTrue());
+        assertThat(calculator.checksBalance(resultOfComparingSides)).isTrue();
     }
 
 }
