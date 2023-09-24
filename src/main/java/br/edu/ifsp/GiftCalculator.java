0e3b0dad4375932347458cd092a349c35763713f
alinirv <alini.vicente@yahoo.com.br>
2023-09-24 15:53:48 -0300

    refactor: increment logic to method.

--- src/main/java/br/edu/ifsp/GiftCalculator.java
+++ src/main/java/br/edu/ifsp/GiftCalculator.java
@@ -1,13 +1,28 @@
 package br.edu.ifsp;
 
 public class GiftCalculator {
+    private int sideA;
+    private int sideB;
 
     public int calculateWeight() {
-        int sumGifts = 0;
-        return sumGifts;
+        int sumWeightGifts = sideA - sideB;
+        return sumWeightGifts;
     }
 
-    public boolean checksBalance() {
+    public boolean checksBalance(int[] weightGifts) {
+
+        for (int i = 0; i < weightGifts.length; i++) {
+            int gifts = weightGifts[i];
+            if (calculateWeight() <= 5) {
+                if (sideA <= sideB) {
+                    sideA = +gifts;
+                } else {
+                    sideB = +gifts;
+                }
+            }else {
+                return false;
+            }
+        }
         return true;
     }
 }
