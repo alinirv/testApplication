d2cdc37005a55acd0949216a66e15a805465c07b
alinirv <alini.vicente@yahoo.com.br>
2023-09-24 14:03:58 -0300

    feat: creates class and methods.

---
+++ src/main/java/br/edu/ifsp/GiftCalculator.java
@@ -0,0 +1,13 @@
+package br.edu.ifsp;
+
+public class GiftCalculator {
+
+    public int calculateWeight() {
+        int sumGifts = 0;
+        return sumGifts;
+    }
+
+    public boolean checksBalance() {
+        return true;
+    }
+}
