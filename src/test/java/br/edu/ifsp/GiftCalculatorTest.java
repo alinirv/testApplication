83c77cbfcd71b4c181dec7a20fe40a6a1680a179
alinirv <alini.vicente@yahoo.com.br>
2023-09-24 13:56:39 -0300

    test: validates balancing

---
+++ src/test/java/br/edu/ifsp/GiftCalculatorTest.java
@@ -0,0 +1,16 @@
+package br.edu.ifsp;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import static org.assertj.core.api.Assertions.*;
+
+class GiftCalculatorTest {
+    @Test
+    @DisplayName("Must balance gifts")
+    void mustBalanceGifts(){
+        GiftCalculator calculator= new GiftCalculator();
+        calculator.calculateWeight();
+        assertThat(calculator.checksBalance().isTrue());
+    }
+
+}
