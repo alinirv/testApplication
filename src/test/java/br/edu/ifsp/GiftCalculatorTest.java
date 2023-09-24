7381a4b8ce24f1884e4512c98584c16d7ab7cf9b
alinirv <alini.vicente@yahoo.com.br>
2023-09-24 16:01:27 -0300

    test: validate unbalance.

--- src/test/java/br/edu/ifsp/GiftCalculatorTest.java
+++ src/test/java/br/edu/ifsp/GiftCalculatorTest.java
@@ -16,4 +16,12 @@ class GiftCalculatorTest {
         int[] weightGifts = {4,6,2};
         assertThat(calculator.checksBalance(weightGifts)).isTrue();
     }
+
+    @Test
+    @DisplayName("Should unbalance gifts")
+    void shouldUnbalanceGifts(){
+        GiftCalculator calculator= new GiftCalculator();
+        int[] weightGifts = {6,6,};
+        assertThat(calculator.checksBalance(weightGifts)).isFalse();
+    }
 }
